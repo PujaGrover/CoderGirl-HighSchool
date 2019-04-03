4a662be6e4cb08300f9755c4d690fef137db5bbc
Puja Grover <puja_grover@yahoo.com>
2019-04-02 22:27:45 -0500

    Week10HwHighSchool: Tested, Commit, Sync, Push

--- HighSchool/Program.cs
+++ HighSchool/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace HighSchool
 {
@@ -6,6 +7,42 @@ namespace HighSchool
     {
         public static void Main()
         {
+            //Create a List off 10 students and add their firstName, lastName, grade and GPA
+            List<Student> student = new List<Student>(){
+
+               new Student() {FirstName = "William", LastName = "Bowser", GPA = 3.5, Grade = 11},
+               new Student() {FirstName = "John", LastName = "Smith", GPA = 3.25, Grade = 9 },
+               new Student() {FirstName = "Anne", LastName = "Smithson", GPA = 4.1, Grade = 12},
+               new Student() {FirstName = "Gerald", LastName = "Robertson", GPA = 3.9, Grade = 13},
+               new Student() {FirstName = "Juliette", LastName = "Brewer", GPA = 4.0, Grade = 10},
+               new Student() {FirstName = "Raphael", LastName = "Katinga", GPA = 2.74, Grade = 12},
+               new Student() {FirstName = "Walter", LastName = "Oblonski", GPA = 2.58, Grade = 11},
+               new Student() {FirstName = "Lucy", LastName = "Kreiger", GPA = 3.00, Grade = 9},
+               new Student() {FirstName = "Ryan", LastName = "Sharp", GPA = 2.75, Grade = 10},
+               new Student() {FirstName = "Laura", LastName = "Walter", GPA = 3.6, Grade = 5},
+
+            };
+
+            Console.Write("My total list has student count of : ");
+            Console.WriteLine(student.Count);
+
+            foreach (Student allstudent in student)
+            {
+                Console.WriteLine(allstudent.FirstName + "       "  + allstudent.GPA + "       " + allstudent.Grade);
+
+            }
+
+            List<Student> myStarStudents = Student.StarStudents(student);
+
+            Console.Write("My star list has student count of : ");
+            Console.WriteLine(myStarStudents.Count);
+
+            foreach (Student starStudent in myStarStudents)
+            {
+                Console.WriteLine(starStudent.Grade + "       "+ starStudent.GPA + "       "+ starStudent.LastName + "       "+ starStudent.FirstName);
+                
+            }
+
             Console.ReadLine();
         }
     }
--- HighSchool/Student.cs
+++ HighSchool/Student.cs
@@ -5,8 +5,76 @@ namespace HighSchool
 {
     public class Student
     {
+        private string firstName;
+        private string lastName;
+        private double studentGPA;
+        private int studentGrade;
+       
+
+        public string FirstName
+        {
+            get { return $"{firstName}"; }
+            set { firstName = value; }
+        }
+        public string LastName
+        {
+            get { return $"{lastName}"; }
+            set { lastName = value; }
+        }
+
+        public double GPA
+        {
+            get { return studentGPA; }
+            set
+            {
+                //studentGPA = value;
+                if (value < 0.0)
+                {
+                    studentGPA = 0;
+                }
+                else if (value > 4)
+                {
+                    studentGPA = 4.0;
+                }
+                else
+                {
+                    studentGPA = value;
+                }
+            }
+        }
+
+        public int Grade
+        {
+            get { return studentGrade; }
+            set
+            {
+                if (value < 1)
+                {
+                    studentGrade = 1;
+                }
+                else if (value > 12)
+                {
+                    studentGrade = 12;
+                }
+                else
+                {
+                    studentGrade = value;
+                }
+            }
+        }
+
+
+
+
         public static List<Student> StarStudents(List<Student> students)
         {
+            var starStudents = students.Where(student => student.studentGPA >= 3 && (student.studentGrade >= 9 && student.studentGrade <= 12));
+            var starStudentsOrdered = starStudents.OrderByDescending(student => student.Grade).ThenByDescending(student => student.GPA).ThenBy(student => student.LastName).ThenBy(student => student.FirstName);
+            
+
+
+            return starStudentsOrdered.ToList();
+            
         }
     }
 }
